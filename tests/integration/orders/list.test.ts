3fd2367e750b4ff9bf8c76219e5bd93af3b1ccce
root <root@DESKTOP-DFB6OFB>
2023-08-14 19:25:47 -0300

    feat: orders list integration tests

--- tests/integration/orders/list.test.ts
+++ tests/integration/orders/list.test.ts
@@ -1,10 +1,20 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import OrderModel from '../../../src/database/models/order.model';
+import app from '../../../src/app';
 
 chai.use(chaiHttp);
 
 describe('GET /orders', function () { 
   beforeEach(function () { sinon.restore(); });
+
+  it('Verifica se retorna todas as ordens cadastradas.', async function () {
+
+   const response = await chai.request(app).get('/orders')
+   console.log(response);
+   expect(response.status).to.be.equal(200)
+
+  })
 
 });
--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -2,8 +2,6 @@ import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../../src/app';
-import mock from '../../mocks/product.mock';
-import ProductModel from '../../../src/database/models/product.model';
 
 chai.use(chaiHttp);
 
@@ -11,8 +9,6 @@ describe('GET /products', function () {
   beforeEach(function () { sinon.restore(); });
 
   it('Verifica se retorna todos os produtos cadastrados.', async function () {
-
-    sinon.stub(ProductModel, 'findAll').resolves()
 
    const response = await chai.request(app).get('/products')
    console.log(response);
